36a8496b33715c81b05a027f8a1ad169d536e83f
Stefania Sgreva <108456339+StefaniaSgreva@users.noreply.github.com>
2025-11-03 19:29:28 +0100

    Add empty CRUD controller function

---
+++ controllers/postController.js
@@ -0,0 +1,30 @@
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function index(req, res) {
+
+}
+
+async function show(req, res){
+
+}
+
+async function store(req, res){
+
+}
+
+async function update(req, res){
+
+}
+
+async function destroy(req, res){
+
+}
+
+module.exports = {
+    index,
+    show,
+    store, 
+    update,
+    destroy
+}
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,22 +4,22 @@ const postController = require('../controllers/postController');
 
 // GET /posts
 // // Recupera tutti i post con eventuali filtri
-router.get('/posts', postController.getPosts);
+router.get('/posts', postController.index);
 
 // GET /posts/:slug
 // Recupera un singolo post tramite slug
-router.get('/posts/:slug', postController.getPostBySlug);
+router.get('/posts/:slug', postController.show);
 
 // POST /posts
 // Crea un nuovo post
-router.post('/posts', postController.createPost);
+router.post('/posts', postController.store);
 
 // PUT /posts/:slug
 // Aggiorna un post tramite slug
-router.put('/posts/:slug', postController.updatePost);
+router.put('/posts/:slug', postController.update);
 
 // DELETE /posts/:slug
 // Elimina un post tramite slug
-router.delete('/posts/:slug', postController.deletePost);
+router.delete('/posts/:slug', postController.destroy);
 
 module.exports = router;
